2d4de43e348f7f92ca0d2215a00cd46cc11dd1fd
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-11-15 17:23:00 -0300

    The function cal_equat_quadratic was improved

--- calculus.rb
+++ calculus.rb
@@ -20,14 +20,33 @@ def calc_equat_quadratic
   x1 = (-coeffic_b + CMath.sqrt(delta) ** 0.5) / (2 * coeffic_a)
   x2 = (-coeffic_b - CMath.sqrt(delta) ** 0.5) / (2 * coeffic_a)
 
+  vert_m_x = -coeffic_b / (2 * coeffic_a)
+  vert_m_y = -delta / (4 * coeffic_a)
+
   puts "Tha roots are: #{x1} and #{x2}"
+
+  if coeffic_a > 0
+
+    # min
+
+    print "The minimum "
+  else
+
+    # max
+
+    print "The maximum "
+  end
+
+  puts "is (#{vert_m_x}, #{vert_m_y})"
 end
 
 def main
   
   # Starting
-  
-  puts "Welcome to the calculus program. Let's learn mathematics!"
+  puts "*************************************************************" 
+  puts "* Welcome to the calculus program. Let's learn mathematics! *"
+  puts "*************************************************************" 
+
   calc_equat_quadratic
 end
 
