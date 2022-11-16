87042c8ddc7bdff1e98600a29519e08221deb613
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-11-16 18:07:19 -0300

    The code was refactored and a new procedure was added

--- calculus.rb
+++ calculus.rb
@@ -2,17 +2,49 @@
 
 require 'cmath'
 
+def print_header
+  
+  puts "*************************************************************" 
+  puts "* Welcome to the calculus program. Let's learn mathematics! *"
+  puts "*************************************************************" 
+
+end
+
+def calc_equat_linear
+
+=begin
+  This function calculates the root
+  of the linear function
+=end
+
+  puts 'We will calculate the root of the linear function!'
+
+  print 'Angle coefficient: '
+  coeffic_a = gets.chomp.to_f
+  print 'Linear coefficient: '
+  coeffic_b = gets.chomp.to_f
+
+  root = -coeffic_b / coeffic_a
+  
+  puts "The root of the equation f(x) = #{coeffic_a}x + #{coeffic_b} is #{root}"
+
+end
+
 def calc_equat_quadratic
   
-  # Calc
+=begin
+  This function calculates the roots
+  and the maximum or minimum point 
+  of the quadratic equation  
+=end
 
-  puts "We will calculate the roots of the quadratic function!"
+  puts 'We will calculate the roots of the quadratic function!'
 
-  print "Type a: "
+  print 'Type a: '
   coeffic_a = gets.chomp.to_f
-  print "Type b: "
+  print 'Type b: '
   coeffic_b = gets.chomp.to_f
-  print "Type c: "
+  print 'Type c: '
   coeffic_c = gets.chomp.to_f
 
   delta = coeffic_b ** 2 - 4 * coeffic_a * coeffic_c
@@ -25,29 +57,44 @@ def calc_equat_quadratic
 
   puts "Tha roots are: #{x1} and #{x2}"
 
-  if coeffic_a > 0
+  if coeffic_a > 0 # min
 
-    # min
+    print 'The minimum '
+  else # max
 
-    print "The minimum "
-  else
-
-    # max
-
-    print "The maximum "
+    print 'The maximum '
   end
 
   puts "is (#{vert_m_x}, #{vert_m_y})"
+
 end
 
 def main
   
-  # Starting
-  puts "*************************************************************" 
-  puts "* Welcome to the calculus program. Let's learn mathematics! *"
-  puts "*************************************************************" 
+  # Start
+  print_header
+  
+  # Choose what you want
 
-  calc_equat_quadratic
+  # Menu
+  puts """
+    1 - Quadratic Equation
+    2 - Linear Equation
+    * - The program is closed
+  """
+
+  print 'What want you make: '
+  user_option = gets.chomp
+
+  case user_option
+  when '1'
+    calc_equat_quadratic
+  when '2'
+    calc_equat_linear
+  else
+    puts "The chosen option #{user_option} doesn't exist"
+  end
+
 end
 
 main
