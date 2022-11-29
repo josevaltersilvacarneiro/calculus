e75137a68be72d40d0d2b1b497e958887f609636
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-11-29 19:12:00 -0300

    The function calc_composite_function that calculates the composition of the functions was added

--- calculus.rb
+++ calculus.rb
@@ -1,8 +1,4 @@
-# Calculus
-
 require 'cmath'
-
-#
 
 def equat_quadratic(a, b = 0, c = 0)
 
@@ -41,6 +37,53 @@ def get_elements(quantity_of_items)
   end
 
   return items
+
+end
+
+def calc_composite_function
+  
+  puts 'We will calculate the composition between two functions!'
+
+  puts """
+    1 → Quadratic Function
+    2 → Linear Function
+  """
+
+  print 'Type of the first function: '
+  type_of_the_first_func = gets.chomp
+  print 'Type of the second function: '
+  type_of_the_second_func = gets.chomp
+
+  case type_of_the_first_func
+  when '1'
+    first_coeffic_a, first_coeffic_b, first_coeffic_c = get_elements(3)
+  when '2'
+    first_coeffic_a, first_coeffic_b = get_elements(2)
+  end
+
+  case type_of_the_second_func
+  when '1'
+    second_coeffic_a, second_coeffic_b, second_coeffic_c = get_elements(3)
+  when '2'
+    second_coeffic_a, second_coeffic_b = get_elements(2)
+  end
+
+  if type_of_the_first_func == '1' and type_of_the_second_func == '2'
+    
+    a = second_coeffic_a ** 2 * first_coeffic_a
+    b = second_coeffic_a * (2 * second_coeffic_b * first_coeffic_a + first_coeffic_b)
+    c = second_coeffic_b ** 2 + second_coeffic_b * first_coeffic_b + first_coeffic_c
+
+    puts "f(g(x)) = #{a}x² + #{b}x + #{c}"
+
+  elsif type_of_the_first_func == '1' and type_of_the_second_func == '2'
+
+    a = second_coeffic_a * first_coeffic_a
+    b = second_coeffic_b * first_coeffic_a + first_coeffic_b
+
+    puts "f(g(x)) = #{a}x + #{b}"
+
+  end
 
 end
 
@@ -98,6 +141,7 @@ def main
   puts """
     1 - Quadratic Equation
     2 - Linear Equation
+    3 - Calculate the composite of two functions
     * - The program is closed
   """
 
@@ -109,6 +153,8 @@ def main
     calc_equat_quadratic
   when '2'
     calc_equat_linear
+  when '3'
+    calc_composite_function
   else
     puts "The chosen option #{user_option} doesn't exist"
   end
